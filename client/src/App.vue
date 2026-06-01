bda4101ce4e45efcb24db62f5b8af3fb3b8dca97
you pity <2586773544@qq.com>
2026-06-01 21:51:33 +0800

    revert: remove particle background

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,7 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import ParticleBackground from './components/ParticleBackground.vue'
 </script>
 
 <template>
-  <ParticleBackground />
-  <div class="app-content">
-    <RouterView />
-  </div>
+  <RouterView />
 </template>
-
-<style>
-.app-content {
-  position: relative;
-  z-index: 1;
-  min-height: 100vh;
-}
-</style>
